2a5077da29a09c11ec3ad5523880d88397543c33
Tasin Farhan <tasinfarhan1016@gmail.com>
2026-03-14 11:22:04 +0600

    Added shader lists

---
+++ src/graphics/shader_list.cpp
@@ -0,0 +1,11 @@
+#include "shader_list.hpp"
+#include <betr/array.hpp>
+#include <utility>
+
+namespace shader {
+template <size_t... Is> inline auto make_shader_array(std::index_sequence<Is...>) {
+  return betr::Array<betr::String, sizeof...(Is)> {get_shader_path_by_id(std::integral_constant<int, (int)Is> {})...};
+}
+
+const auto list = make_shader_array(std::make_index_sequence<count> {});
+} // namespace shader
