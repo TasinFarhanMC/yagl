c54abf74ff7af687c316df8a99e769090d869fd0
Tasin Farhan <tasinfarhan1016@gmail.com>
2026-03-24 20:47:24 +0600

    Added clay to cmake

--- shaders/rect.vert.glsl
+++ shaders/rect.vert.glsl
@@ -7,6 +7,6 @@ layout(location = 2) in uvec2 scale;
 const vec2 dim = vec2(160.0, 90.0);
 
 void main() {
-    vec2 world_pos = base_pos * vec2(scale) + pos;
-    gl_Position = vec4(world_pos * 2.0 / dim - 1.0, 0.0, 1.0);
+  vec2 world_pos = base_pos * vec2(scale) + pos;
+  gl_Position = vec4(world_pos * 2.0 / dim - 1.0, 0.0, 1.0);
 }
