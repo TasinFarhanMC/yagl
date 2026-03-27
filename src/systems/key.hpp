2068cb686f92321800d798825f661dfe42d2203d
Tasin Farhan <tasinfarhan1016@gmail.com>
2026-03-27 14:13:33 +0600

    Improved key state fetch

--- src/systems/key.hpp
+++ src/systems/key.hpp
@@ -16,9 +16,18 @@ extern betr::Array<char, GLFW_KEY_LAST + 1> mods;
 void callback(GLFWwindow *window, int key, int scancode, int action, int mods);
 
 template <typename... States> bool has_state(int key, States... states) { return ((key::state[key] == states) || ...); }
+template <typename... States> bool has_state(int key, int mods, States... states) {
+  return ((key::state[key] == states) || ...) && key::mods[key] & mods;
+}
 
 template <typename... States> bool had_state(int key, States... states) {
   const bool result = ((key::state[key] == states) || ...);
+  key::state[key] = State::None;
+  return result;
+}
+
+template <typename... States> bool had_state(int key, int mods, States... states) {
+  const bool result = ((key::state[key] == states) || ...) && key::mods[key] & mods;
   key::state[key] = State::None;
   return result;
 }
