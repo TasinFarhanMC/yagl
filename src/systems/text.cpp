ce7ea1ccfb900a57e955a7050c46fc0bd424f677
Tasin Farhan <tasinfarhan1016@gmail.com>
2026-03-27 23:55:35 +0600

    Added seperate key states for text

--- src/systems/text.cpp
+++ src/systems/text.cpp
@@ -3,10 +3,18 @@
 #include "text.hpp"
 
 namespace text {
-betr::String *string = nullptr;
-int max_size = 255;
+String *string = nullptr;
+int max_size = -1;
+
+Array<key::State, GLFW_KEY_LAST + 1> key_state;
+Array<char, GLFW_KEY_LAST + 1> key_mods;
 
 void callback(GLFWwindow *window, unsigned int codepoint) {
   if (codepoint <= 127 && string && string->size() < max_size) { string->push_back(codepoint); }
+}
+
+void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
+  text::key_state[key] = static_cast<key::State>(action);
+  text::key_mods[key] = mods;
 }
 } // namespace text
