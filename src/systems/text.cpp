97358ffdcc86cfb8359eb3ab787d50c1083cd1f4
Tasin Farhan <tasinfarhan1016@gmail.com>
2026-03-29 00:23:03 +0600

    Removed key callback from text, added text case transforms

--- src/systems/text.cpp
+++ src/systems/text.cpp
@@ -5,16 +5,19 @@
 namespace text {
 String *string = nullptr;
 int max_size = -1;
-
-Array<key::State, GLFW_KEY_LAST + 1> key_state;
-Array<char, GLFW_KEY_LAST + 1> key_mods;
+Case case_trans = Case::All;
 
 void callback(GLFWwindow *window, unsigned int codepoint) {
-  if (codepoint <= 127 && string && string->size() < max_size) { string->push_back(codepoint); }
-}
+  if (!string) { return; }
 
-void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
-  text::key_state[key] = static_cast<key::State>(action);
-  text::key_mods[key] = mods;
+  char c = static_cast<char>(codepoint);
+
+  switch (case_trans) {
+  case Case::Upper: c = (c >= 'a' && c <= 'z') ? (c - 32) : c; break;
+  case Case::Lower: c = (c >= 'A' && c <= 'Z') ? (c + 32) : c; break;
+  case Case::All: break;
+  }
+
+  string->push_back(c);
 }
 } // namespace text
--- src/systems/text.hpp
+++ src/systems/text.hpp
@@ -4,29 +4,15 @@
 #include <systems/key.hpp>
 
 namespace text {
+enum class Case : char {
+  Upper,
+  Lower,
+  All
+};
+
 extern betr::String *string;
 extern int max_size;
+extern Case case_trans;
 
-extern betr::Array<key::State, GLFW_KEY_LAST + 1> key_state;
-extern betr::Array<char, GLFW_KEY_LAST + 1> key_mods;
-
-void callback(GLFWwindow *window, unsigned int codepoint);
-void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods);
-
-template <typename... States> bool has_state(int key, States... states) { return ((key::state[key] == states) || ...); }
-template <typename... States> bool has_state(int key, int mods, States... states) {
-  return ((key::state[key] == states) || ...) && key::mods[key] & mods;
-}
-
-template <typename... States> bool had_state(int key, States... states) {
-  const bool result = ((text::key_state[key] == states) || ...);
-  text::key_state[key] = key::State::None;
-  return result;
-}
-
-template <typename... States> bool had_state(int key, int mods, States... states) {
-  const bool result = ((text::key_state[key] == states) || ...) && text::key_mods[key] & mods;
-  text::key_state[key] = key::State::None;
-  return result;
-}
+extern void callback(GLFWwindow *window, unsigned int codepoint);
 } // namespace text
